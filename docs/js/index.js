0f9b0e0e9d7683db72b21b0b6d4eed00e29a7b14
t-takahashi <tkhstol.929@gmail.com>
2023-03-12 16:27:08 +0900

    update html, js

---
+++ docs/js/index.js
@@ -0,0 +1,6 @@
+window.addEventListener("scroll", () => {
+    console.log(document.querySelector("#aboutMe").scrollTop);
+    console.log(document.documentElement.scrollTop);
+    // console.log(document.querySelector("#aboutMe"));
+    // console.log(document.getElementById("aboutMe"));
+})
