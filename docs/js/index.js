a4f9e7618b45e9f536a17b5aba0015a3e09d406b
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-04-13 23:25:45 +0900

    Fix styling and add scroll event listener

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,6 +1,20 @@
-window.addEventListener("scroll", () => {
-    console.log(document.querySelector("#aboutMe").scrollTop);
-    console.log(document.documentElement.scrollTop);
-    // console.log(document.querySelector("#aboutMe"));
+// JavaScript
+window.addEventListener('scroll', function() {
+    const aboutMeElement = document.getElementById('aboutMe'); // "About TOL"の要素のIDを指定
+    const scrollPosition = window.scrollY;
+
+    console.log(`scrollPosition: ${scrollPosition}`);
+
+    if (scrollPosition > 200) { // 100pxスクロールしたら色を変更
+        aboutMeElement.classList.add('color-change');
+    } else {
+        aboutMeElement.classList.remove('color-change');
+    }
+});
+
+// window.addEventListener("scroll", () => {
+//     console.log(document.querySelector("#aboutMe").scrollTop);
+//     console.log(document.documentElement.scrollTop);
+//     // console.log(document.querySelector("#aboutMe"));
     // console.log(document.getElementById("aboutMe"));
-})
+// });
