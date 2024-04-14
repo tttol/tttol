b32fe5f721558625c1d58aec35a13fad02a1f74f
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-04-14 22:52:48 +0900

    Update custom.css and index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,20 +1,27 @@
-// JavaScript
+// window.addEventListener('scroll', function() {
+//     const aboutMeElement = document.getElementById('aboutMe');
+//     const scrollPosition = window.scrollY;
+//     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+//     // スクロール位置に応じて色を変更
+//     const topColor = scrollPosition / height > 0.5 ? '#ffffff' : '#0000ff';
+//     const bottomColor = scrollPosition / height > 0.5 ? '#0000ff' : '#ffffff';
+
+//     aboutMeElement.style.background = `linear-gradient(to bottom, ${topColor}, ${bottomColor})`;
+// });
 window.addEventListener('scroll', function() {
-    const aboutMeElement = document.getElementById('aboutMe'); // "About TOL"の要素のIDを指定
+    const aboutMeElement = document.getElementById('aboutMe');
     const scrollPosition = window.scrollY;
-
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     console.log(`scrollPosition: ${scrollPosition}`);
+    console.log(`height: ${height}`);
 
-    if (scrollPosition > 200) { // 100pxスクロールしたら色を変更
-        aboutMeElement.classList.add('color-change');
+    // スクロール位置に応じてクラスを切り替え
+    if (scrollPosition / height > 0.5) {
+        aboutMeElement.classList.remove('from-[#ffffff]', 'to-[#0000ff]');
+        aboutMeElement.classList.add('from-[#0000ff]', 'to-[#ffffff]');
     } else {
-        aboutMeElement.classList.remove('color-change');
+        aboutMeElement.classList.remove('from-[#0000ff]', 'to-[#ffffff]');
+        aboutMeElement.classList.add('from-[#ffffff]', 'to-[#0000ff]');
     }
 });
-
-// window.addEventListener("scroll", () => {
-//     console.log(document.querySelector("#aboutMe").scrollTop);
-//     console.log(document.documentElement.scrollTop);
-//     // console.log(document.querySelector("#aboutMe"));
-    // console.log(document.getElementById("aboutMe"));
-// });
